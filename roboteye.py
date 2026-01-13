1604de439afccf300b9c6baf9b56606dc043d0fa
Siti Aisyah Binti Jaafar <sitiaisyahjaafar@MacBook-Air-4.local>
2026-01-13 15:47:10 +0800

    from lappy

---
+++ roboteye.py
@@ -0,0 +1,19 @@
+import pybullet as p
+import pybullet_data
+import time
+import numpy as np
+
+p.connect(p.GUI) # or p.DIRECT for no GUI
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.loadURDF("plane.urdf")
+robot_id = p.loadURDF("kuka_iiwa/model.urdf", [0, 0, 0]) # Load your robot model
+
+# usually this remain constant:
+width, height = 480, 360
+fov = 60
+aspect = width / height
+nearVal, farVal = 0.01, 10
+projectionMatrix = p.computeProjectionMatrixFOV(fov, aspect, nearVal, farVal)
+
+
+
